5fa3fdd2fc063891bf38a70fd97e835a7e84b0e2
jingahhh <117644602+jwang006@users.noreply.github.com>
2023-03-01 23:17:24 +0000

    css

---
+++ javascript/sketch.js
@@ -0,0 +1,23 @@
+
+//Main sketch below
+// an array to store the creatures
+let creatures = [];
+let food = [];
+
+function setup() {
+ 
+  // canvas = createCanvas(windowWidth,windowHeight);
+  // canvas.parent("sketch-container"); //move our canvas inside this HTML element
+  // background(255,150);
+}
+
+function draw() {
+  
+
+  
+}
+function windowResized() {
+
+  resizeCanvas(windowWidth, windowHeight);
+
+}
